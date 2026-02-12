afa318e03b418218ef410a4ee01d9c7068b3ae49
kumarpindia <technologistpuneet@gmail.com>
2026-02-12 13:14:19 +0530

    removed redundant code

--- graphs/edgeListToadjacencyList.py
+++ graphs/edgeListToadjacencyList.py
@@ -29,42 +29,6 @@ def convert_edge_list_to_adjacency_list(n, edges):
     
     return adj_list_final
 
-"""
-        This approach is not working because the vertices are not in sorted order.
-        For example, if edges = [[1, 2], [0, 1]], then when i = 0, left_vertex = 1 and right_vertex = 2.
-        So, we will create adjacency list for vertex 1 and vertex 2. But when i = 1, left_vertex = 0 and right_vertex = 1.
-        Now, we will create adjacency list for vertex 0. But vertex 1 is already done. So, we will skip it.
-        Finally, the adjacency list will be [[], [0, 2], [1], []] which is incorrect.
-        The correct adjacency list should be [[1], [0, 2], [1], []].
-        Hence, we need to iterate through all edges for each vertex to create the adjacency list.
-        
-        left_vertex = edges[i][0]
-        right_vertex = edges[i][1]
-        adj_list_temp = []
-            
-        if left_vertex not in adj_list_made_of:
-            for edge in edges:
-                if left_vertex == edge[0]:
-                    adj_list_temp.append(edge[1])
-                if left_vertex == edge[1]:
-                    adj_list_temp.append(edge[0])
-            adj_list_temp.sort()
-            adj_list_final.insert(left_vertex, adj_list_temp)
-            adj_list_made_of.append(left_vertex)
-        
-        adj_list_temp = []
-
-        if right_vertex not in adj_list_made_of:
-            for edge in edges:
-                if right_vertex == edge[0]:
-                    adj_list_temp.append(edge[1])
-                if right_vertex == edge[1]:
-                    adj_list_temp.append(edge[0])
-            adj_list_temp.sort()
-            adj_list_final.insert(right_vertex, adj_list_temp)
-            adj_list_made_of.append(right_vertex)
-""" 
-
 
 #print(convert_edge_list_to_adjacency_list(5, [[0, 1], [1, 4], [1, 2], [1, 3], [3, 4]]))
 #print(convert_edge_list_to_adjacency_list(2, [[0, 1]]))
